fa79c6608f8b44714bf7de56eace84c7fe9bd3b9
iliass-wakkar <iliasswakkar.wip@gmail.com>
2024-11-02 12:33:41 +0100

    fix hosting problemes

--- src/head/head.php
+++ src/head/head.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 ?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
--- src/homepage/index.php
+++ src/homepage/index.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
 <?php require_once("../head/head.php") ?>
 <body>
     <header>
