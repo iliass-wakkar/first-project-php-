6f2a009e59dd32b60ceb2827728b0c7f3c32b7f7
iliass-wakkar <iliasswakkar.wip@gmail.com>
2024-11-03 13:48:47 +0100

    Update nav.php

--- src/nav/nav.php
+++ src/nav/nav.php
@@ -1,31 +1,31 @@
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary" style="position: fixed; top: 0; width: 100%; z-index: 1000;" >
   <div class="container-fluid">
-    <a class="navbar-brand" href="/var/www/html/index.php">php projet</a>
+    <a class="navbar-brand" href="<?php echo ROOT_PATH?>index.php">php projet</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/var/www/html/index.php">Accueil</a>
+          <a class="nav-link active" aria-current="page" href="<?php echo ROOT_PATH ?>'index.php">Accueil</a>
         </li>
         <?php 
     if(isset($_SESSION['client'])){
         echo '<li class="nav-item dropdown">'.
         $_SESSION['client'].
         '<ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="/var/www/html/login/let.php">singout</a></li>
+        <li><a class="dropdown-item" href="'.ROOT_PATH.'login/let.php">singout</a></li>
       </ul></li>';
     } elseif(isset($_SESSION['admin'])){
         echo '<li class="nav-item dropdown">'.
         $_SESSION['admin'].
         '<ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="/var/www/html/login/let.php">singout</a></li>
+        <li><a class="dropdown-item" href="'.ROOT_PATH.'login/let.php">singout</a></li>
       </ul></li>';
       //------------------------------------------
       echo "<li class='nav-item'>
-          <a class='nav-link' href='/var/www/html/gestionadmin/gestion.php'>Gestion Clients</a>
+          <a class='nav-link' href='".ROOT_PATH."gestionadmin/gestion.php'>Gestion Clients</a>
         </li>";
       
 
@@ -35,8 +35,8 @@
                   S\'identifier/inscription
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="/var/www/html/login/login.php">S\'identifier</a></li>
-                  <li><a class="dropdown-item" href="/var/www/html/inscrire/inscrirepage.php">Inscription</a></li>
+                  <li><a class="dropdown-item" href="'.ROOT_PATH.'login/login.php">S\'identifier</a></li>
+                  <li><a class="dropdown-item" href="'.ROOT_PATH.'inscrire/inscrirepage.php">Inscription</a></li>
                 </ul>
               </li>';
     }
@@ -49,7 +49,7 @@
         <li class="nav-item">
           <a class="nav-link" href="#">À propos de nous</a>
         </li>
-        <form method="post" action="/var/www/html/verifiersearch/verifiersearch.php" class="px-4 d-flex" role="search">
+        <form method="post" action="<?php echo ROOT_PATH ?>verifiersearch/verifiersearch.php" class="px-4 d-flex" role="search">
         <input name="searchval" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button name="search" class="btn btn-outline-success" type="submit">Search</button>
       </form>
@@ -64,14 +64,14 @@
         $pdo = conx();
         $count=$cart->count_cart($pdo, $_SESSION['id_client']);
       
-        $_SESSION["cart"] = "<a href='/var/www/html/card/cart.php'><i class='px-3 fa-solid fa-cart-shopping'><span class='badge bg-danger rounded-circle'>
+        $_SESSION["cart"] = "<a href='".ROOT_PATH."card/cart.php'><i class='px-3 fa-solid fa-cart-shopping'><span class='badge bg-danger rounded-circle'>
 <span class='small'>".$count."</span></span></i></a>";
         echo $_SESSION['cart'];}
     } elseif(isset($_SESSION['nbcart'])) {
-        echo "<a href='/var/www/html/card/cart.php'><i class='px-3 fa-solid fa-cart-shopping'><span class='badge bg-danger rounded-circle'>
+        echo "<a href='".ROOT_PATH."card/cart.php'><i class='px-3 fa-solid fa-cart-shopping'><span class='badge bg-danger rounded-circle'>
         <span class='small'>".$_SESSION['nbcart']."</span></span></i></a>";
     }else
-    echo "<a href='/var/www/html/card/cart.php'><i class='px-3 fa-solid fa-cart-shopping'><span class='badge bg-danger rounded-circle'>
+    echo "<a href='".ROOT_PATH."card/cart.php'><i class='px-3 fa-solid fa-cart-shopping'><span class='badge bg-danger rounded-circle'>
         <span class='small'>0</span></span></i></a>";
         
        ?>
